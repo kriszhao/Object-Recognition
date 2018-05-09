facc12d79dee2fa43ab2338e6d4a9e7b25d2bfe5
Vi Le <vianle@berkeley.edu>
2018-05-09 10:37:13 -0700

    took out android app

--- webcamApp/main.py
+++ webcamApp/main.py
@@ -2,20 +2,21 @@ import cv2
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 from detect import detect_naive
+from preprocess import preprocess
 
 def grab_frame(cap):
     ret,frame = cap.read()
     return cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
 
 def draw_box(img):
-	part, coor = detect_naive(img)
+	obj, coor = detect_naive(img)
 	y, x = coor
 	currentAxis = plt.gca()
 	rect = Rectangle((x.start, y.start),\
 	 x.stop - x.start, y.stop - y.start, linewidth=5, \
 	 edgecolor="red", fill=False)
 	currentAxis.add_patch(rect)
-	return rect
+	return rect, obj
 
 def main():
 	cap = cv2.VideoCapture(0)
@@ -32,8 +33,12 @@ def main():
 		im.set_data(curr)
 		curr_gray = cv2.cvtColor(curr, cv2.COLOR_BGR2GRAY)
 		box.set_visible(False)
-		box = draw_box(curr_gray)
+		box, obj = draw_box(curr_gray)
 		box.set_visible(True)
+
+		scaled_im = cv2.resize(obj, (32,32))
+		variations = preprocess(scaled_im)
+
 		plt.pause(0.2)
 
 	# plt.ioff()
