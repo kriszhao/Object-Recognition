163ba1ac3c4746a5e59da38fd3928ccfe77be3f8
Vi Le <vianle@berkeley.edu>
2018-05-09 15:06:00 -0700

    preprocessing stuff

--- webcamApp/main.py
+++ webcamApp/main.py
@@ -83,9 +83,6 @@ def main():
 	box = Rectangle((1,1),1,1)
 	box.set_visible(False)
 
-	# scaled_im = cv2.resize(grab_frame(cap), (32,32))
-	# plt.imshow(scaled_im)
-
 	while plt.get_fignums():
 		curr = grab_frame(cap)
 		im.set_data(curr)
@@ -104,9 +101,6 @@ def main():
 		#print(classify_array(scaled_im))
 
 		plt.pause(0.2)
-
-	# plt.ioff()
-	# plt.show()
 
 if __name__ == "__main__":
 	main()
--- webcamApp/preprocess.py
+++ webcamApp/preprocess.py
@@ -1,26 +1,49 @@
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
 def preprocess(im, samples):
 	variations = []
 	variations.append(im)
 
-	# gaussian filter
-	for i in range(samples):
-		kernNum = i
-		variations.append(cv2.GaussianBlur(im,(kernNum,kernNum),0))
+	# # gaussian filter
+	# for i in range(samples):
+	# 	kernNum = i
+	# 	variations.append(cv2.GaussianBlur(im,(kernNum,kernNum),0))
 
 	# median filter
 	for i in range(samples):
 		kernNum = i
 		variations.append(cv2.medianBlur(im,kernNum))
 
-	# hues
+	# make hue more yellow (natural light)
+	for i in range(samples):
+		copy = np.copy(im)
+		gain = 1./samples
+		copy[:,:,0] = copy[:,:,0]*(1.-gain*i)
+		variations.append(copy)
 
 	# saturation
 
 	# contrast
 
 	# rotate
+	for i in range(samples):
+		deg = 360/samples
+		rows,cols,rgb = im.shape
+		M = cv2.getRotationMatrix2D((cols/2,rows/2),i*deg,1)
+		dst = cv2.warpAffine(im,M,(cols,rows))
+		variations.append(dst)
 
 	return variations
+
+def runex():
+	im = cv2.imread("./cut_image/result_0.jpg")
+	cv2.imshow("img",im)
+	cv2.waitKey(0)
+	variations = preprocess(im, 5)
+	for variation in variations:
+		cv2.imshow("img",variation)
+		cv2.waitKey(0)
+
+runex()
