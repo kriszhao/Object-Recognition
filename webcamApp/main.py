0d064f556156602aae59c5d65ad7d19739275abf
Vi Le <vianle@berkeley.edu>
2018-05-09 11:04:04 -0700

    fixed rgb?

--- webcamApp/main.py
+++ webcamApp/main.py
@@ -9,34 +9,38 @@ def grab_frame(cap):
     return cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
 
 def draw_box(img):
-	obj, coor = detect_naive(img)
+	gray, coor = detect_naive(img)
 	y, x = coor
 	currentAxis = plt.gca()
 	rect = Rectangle((x.start, y.start),\
 	 x.stop - x.start, y.stop - y.start, linewidth=5, \
 	 edgecolor="red", fill=False)
 	currentAxis.add_patch(rect)
-	return rect, obj
+	return rect, coor
 
 def main():
 	cap = cv2.VideoCapture(0)
 
 	im = plt.imshow(grab_frame(cap))
-
-	plt.ion()
+	# plt.text(0,0,"item name")
+	# plt.ion()
 
 	box = Rectangle((1,1),1,1)
 	box.set_visible(False)
+
+	# scaled_im = cv2.resize(grab_frame(cap), (32,32))
+	# plt.imshow(scaled_im)
 
 	while True:
 		curr = grab_frame(cap)
 		im.set_data(curr)
 		curr_gray = cv2.cvtColor(curr, cv2.COLOR_BGR2GRAY)
 		box.set_visible(False)
-		box, obj = draw_box(curr_gray)
+		box, slices = draw_box(curr_gray)
+		slice_x, slice_y = slices
 		box.set_visible(True)
 
-		scaled_im = cv2.resize(obj, (32,32))
+		scaled_im = cv2.resize(curr[slice_x, slice_y], (32,32))
 		variations = preprocess(scaled_im)
 
 		plt.pause(0.2)
